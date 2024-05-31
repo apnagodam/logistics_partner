adea3c90a1fa5b76d1520ae8d7b234490a26a442
apnagodamdeveloper <apnagodamdeveloper@gmail.com>
2024-05-31 18:55:24 +0530

    base update

---
+++ lib/Domain/providers/dio/dio_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dio_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dioInstanceHash() => r'51fe4d7bcebafd96f13c189843b6ad7d2834f4eb';
+
+/// See also [dioInstance].
+@ProviderFor(dioInstance)
+final dioInstanceProvider = AutoDisposeProvider<Dio>.internal(
+  dioInstance,
+  name: r'dioInstanceProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$dioInstanceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef DioInstanceRef = AutoDisposeProviderRef<Dio>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
